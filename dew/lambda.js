52abe743e25d3415203a20797f686cd206420cb9
Indunil1992 <indunil@adroitlogic.com>
2019-10-23 14:03:17 +0530

    2

--- dew/lambda.js
+++ dew/lambda.js
@@ -1,15 +1,25 @@
 let AWS = require('aws-sdk');
-const sns = new AWS.SNS();
+let SL_AWS = require('slappforge-sdk-aws');
+const sqs = new SL_AWS.SQS(AWS);
 
 exports.handler = function (event, context, callback) {
-    sns.publish({
-        Message: 'hi ',
-        MessageAttributes: {},
-        MessageStructure: 'String',
-        TopicArn: 'arn:aws:sns:us-east-1:318300609668:new123'
+    sqs.receiveMessage({
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/dss`,
+        AttributeNames: ['All'],
+        MaxNumberOfMessages: '1',
+        VisibilityTimeout: '30',
+        WaitTimeSeconds: '0'
     }).promise()
-        .then(data => {
-            // your code goes here
+        .then(receivedMsgData => {
+            if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
+                let receivedMessages = receivedMsgData.Messages;
+                receivedMessages.forEach(message => {
+                    // your logic to access each message through out the loop. Each message is available under variable message 
+                    // within this block
+                });
+            } else {
+                // No messages to process
+            }
         })
         .catch(err => {
             // error handling goes here
